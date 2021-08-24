5e91fd10307e12c2c54ef3c49fb25a13bb7a9094
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-24 08:46:26 +0530

    ADDED: method to play one game and check if gambler wins or not

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -4,9 +4,21 @@ public class GamblerSimulation {
 	
 	public static final int TOTAL_STAKE=100;
 	public static final int BET_PER_GAME=1;
-
+	
+	public static void playGame() {
+		double game=Math.random();
+		if(game>0.5) {
+			System.out.println("Won the game");
+		}
+		else {
+			System.out.println("Lost the game");
+		}
+	}
 	public static void main(String[] args) {
 		
+		System.out.println("Total stake="+TOTAL_STAKE);
+		System.out.println("Bet per game="+BET_PER_GAME);
+		playGame();
 
 	}
 

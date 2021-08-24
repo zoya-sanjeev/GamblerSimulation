74dd027f51cfd276b470d226ab7248531eda2a4e
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-24 09:55:48 +0530

    ADDED: method to calculate total amount over a period of days

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -8,23 +8,39 @@ public class GamblerSimulation {
 	public static int playGame(int availableBalance) {
 		double game=Math.random();
 		if(game>0.5) {
-			System.out.println("Won the game");
 			return availableBalance+BET_PER_GAME;
 		}
 		else {
-			System.out.println("Lost the game");
 			return availableBalance-BET_PER_GAME;
 		}
+	}
+	public static int gameResigning(int availableBalance) {
+		while(availableBalance<TOTAL_STAKE+0.5*TOTAL_STAKE && availableBalance>TOTAL_STAKE-0.5*TOTAL_STAKE) {
+			availableBalance=playGame(availableBalance);
+			
+			}
+			System.out.println("Available Balance:"+availableBalance);
+			return availableBalance;
+		
+	}
+	public static int gamePlayForDays(int noOfDays, int availableBalance) {
+		int totalAmount=availableBalance;
+		for(int day=1; day<=noOfDays;day++) {
+			totalAmount+=gameResigning(totalAmount);
+		}
+		return totalAmount;
+		
 	}
 	public static void main(String[] args) {
 		
 		System.out.println("Total stake="+TOTAL_STAKE);
 		System.out.println("Bet per game="+BET_PER_GAME);
 		int availableBalance=TOTAL_STAKE;
-		while(availableBalance<=TOTAL_STAKE+0.5*TOTAL_STAKE && availableBalance>=TOTAL_STAKE-0.5*TOTAL_STAKE) {
-		availableBalance=playGame(availableBalance);
-		System.out.println("Balance available:"+availableBalance);
-		}
+		System.out.println("Starting Balance:"+availableBalance);
+		int noOfDays=20;
+		int totalAmount=0;
+		totalAmount=gamePlayForDays(noOfDays, availableBalance);
+		System.out.println("Amount after "+ noOfDays+" days of play:"+totalAmount);
 		
 
 	}

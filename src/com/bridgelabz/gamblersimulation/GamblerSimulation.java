53c5b1c7fd5493aad4170c6a44d563a0513cb838
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-24 09:04:58 +0530

    MODIFIED: variable for available balance

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -5,20 +5,24 @@ public class GamblerSimulation {
 	public static final int TOTAL_STAKE=100;
 	public static final int BET_PER_GAME=1;
 	
-	public static void playGame() {
+	public static int playGame(int availableBalance) {
 		double game=Math.random();
 		if(game>0.5) {
 			System.out.println("Won the game");
+			return availableBalance+BET_PER_GAME;
 		}
 		else {
 			System.out.println("Lost the game");
+			return availableBalance-BET_PER_GAME;
 		}
 	}
 	public static void main(String[] args) {
 		
 		System.out.println("Total stake="+TOTAL_STAKE);
 		System.out.println("Bet per game="+BET_PER_GAME);
-		playGame();
+		int availableBalance=TOTAL_STAKE;
+		availableBalance=playGame(availableBalance);
+		System.out.println(availableBalance);
 
 	}
 

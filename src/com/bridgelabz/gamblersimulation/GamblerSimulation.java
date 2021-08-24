c3500d8f844759f9929a3925557fc96b425b1673
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-24 08:24:17 +0530

    ADDED: constants for total stake and bet per day

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -1,9 +1,12 @@
 package com.bridgelabz.gamblersimulation;
 
 public class GamblerSimulation {
+	
+	public static final int TOTAL_STAKE=100;
+	public static final int BET_PER_GAME=1;
 
 	public static void main(String[] args) {
-		System.out.println("Gambler Simulation");
+		
 
 	}
 
